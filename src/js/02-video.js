a02e0586135e4fffbc4a50e906f07172dd60feb5
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2023-11-20 12:06:16 +0200

    Add task 2

---
+++ src/js/02-video.js
@@ -0,0 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+
+const player = new Player(iframe);
+
+const updateLocalStorage = throttle(async () => {
+  const currentTime = await player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', updateLocalStorage);
+player.ready().then(() => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  } else {
+  }
+});
